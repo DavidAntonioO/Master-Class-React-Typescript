2d3ce9dc295d4e63335e84652cfc6622a444680e
DavidAntonioO <david-antonio.olmos@atos.net>
2022-02-22 11:40:35 -0800

    Logical OR and AND - Hands On

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
-for(let i =0; i<3; i++){
-    (function test(o){
-        alert(o);
-    })(i)
-}
+const test = 5 && 6; //6
+
+const sample = 0 && false && null; //0
+
+console.log(test);
+
+console.log(sample);
