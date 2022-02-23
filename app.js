22b03ab331aef5a717270e8e20f5048dde7d4f95
DavidAntonioO <david-antonio.olmos@atos.net>
2022-02-23 13:15:51 -0800

    This Object

---
+++ app.js
@@ -0,0 +1,24 @@
+class Printer{
+    name = "I am Printer Class"
+
+    printName(){
+        console.log('printName: ', this.name);
+    }
+
+    printNameArrowFn = () => {
+        console.log('printNameArrowFn:' , this.name);
+    }
+}
+
+const myPrinter = new Printer();
+//myPrinter.printName();
+//myPrinter.printNameArrowFn();
+
+const customPrinter = {
+    name: 'I am Custom Printer',
+    printName: myPrinter.printName,
+    printNameArrowFn: myPrinter.printNameArrowFn,
+}
+
+//customPrinter.printName();
+customPrinter.printNameArrowFn();
